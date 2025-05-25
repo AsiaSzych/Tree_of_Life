0e38adafbbe8c7beb590663cd39efbee898d836b
AsiaSzych <joannaszych@gmail.com>
2025-05-25 11:23:28 +0200

    small refactores and png generation

--- reference_implementation/phylogenetic_tree.py
+++ reference_implementation/phylogenetic_tree.py
@@ -6,15 +6,6 @@ organisms_json_file_path = "../starter_code/organisms.json"
 newick_txt_file = f"./tree{BLOSUM_VERSION}_newick.nw"
 newick_distance_txt_file = f"./tree{BLOSUM_VERSION}_newick_with_distance.nw"
 
-with open(nw_json_file_path, 'r') as j:
-    nw_scores = json.loads(j.read())
-
-with open(organisms_json_file_path, 'r') as j:
-    organisms = json.loads(j.read())
-
-nw_scores_sorted = {k: v for k, v in sorted(nw_scores.items(), key=lambda item: item[1], reverse=True)}
-
-
 class Node:
 
     def __init__(self, name, value, parent=None, root=None, left=None, right=None):
@@ -39,13 +30,13 @@ class Tree:
 
     def insert(self, node, node_name, node_value, left_child, right_child):
 
-        #if tree is empty , return a root node
         if node is None:
             return self.createNode(name=node_name, value=node_value, left=left_child, right=right_child)
 
         return node
     
     def change_root_for_all_children(self, current_node, new_root, fast_track):
+
         current_node.set_parent(new_root)
         fast_track[current_node.name] = new_root
         if current_node.left:
@@ -56,6 +47,7 @@ class Tree:
         return fast_track
 
     def make_newick(self, node, distance=False):
+
         newick = ""
         if distance:
             newick = self.traverse_newick_with_distance(node, newick)
@@ -64,6 +56,7 @@ class Tree:
         return f"{newick};"
     
     def traverse_newick(self, root_node, newick):
+
         if root_node.left and not root_node.right:
             newick = f"(,{self.traverse_newick(root_node.left, newick)}){root_node.name}"
         elif not root_node.left and root_node.right:
@@ -77,6 +70,7 @@ class Tree:
         return newick
 
     def traverse_newick_with_distance(self, root_node, newick):
+
         if root_node.left and not root_node.right:
             newick = f"(,{self.traverse_newick_with_distance(root_node.left, newick)}):{root_node.value}"
         elif not root_node.left and root_node.right:
@@ -91,21 +85,16 @@ class Tree:
 
 class UnionFind:
     def __init__(self, keys):
-      
-        # Initialize the parent array with each 
-        # element as its own representative
+
         self.parent = {}
         for key in keys:
             self.parent[key] = key
         
     def find(self, key):
-      
-        # If i itself is root or representative
+
         if self.parent[key] == key:
             return key
           
-        # Else recursively find the representative 
-        # of the parent
         return self.find(self.parent[key])
     
     def unite(self, key1, key2):
@@ -121,14 +110,10 @@ class UnionFind:
         self.parent[irep] = jrep
 
 
-union_find_structure = UnionFind(organisms.keys())
+def init_tracking_cache(init_dict, main_tree, nw_scores_sorted):
 
-treeOfLife = Tree()
-
-max_distance = nw_scores_sorted[list(nw_scores_sorted.keys())[0]]
-max_distance = max_distance + 50
-
-def init_tracking_tree(init_dict, main_tree):
+    max_distance = nw_scores_sorted[list(nw_scores_sorted.keys())[0]]
+    max_distance = max_distance + 50
     tracking_dict = {}
     keys = list(init_dict.keys())
     for key in keys:
@@ -137,31 +122,47 @@ def init_tracking_tree(init_dict, main_tree):
 
     return tracking_dict
 
-tracking_tree = init_tracking_tree(organisms, treeOfLife)
-
-node_counter = 1
-for k, v in nw_scores_sorted.items():
-    species = k.split("_")
-    specie1 = species[0]
-    specie2 = species[1]
-    species1_root_node = tracking_tree[specie1]
-    species2_root_node = tracking_tree[specie2]
-    species1_root = union_find_structure.find(specie1)
-    species2_root = union_find_structure.find(specie2)
-
-    if species1_root!=species2_root:
-        union_find_structure.unite(specie1, specie2)
-        node_counter = node_counter+1
-        new_parent_node = treeOfLife.createNode(name = v, value = v, left=species1_root_node, right=species2_root_node)
-        tracking_tree = treeOfLife.change_root_for_all_children(current_node=species1_root_node, new_root=new_parent_node, fast_track = tracking_tree)
-        tracking_tree = treeOfLife.change_root_for_all_children(current_node=species2_root_node, new_root=new_parent_node, fast_track = tracking_tree)
 
 
-tree_newick = treeOfLife.make_newick(new_parent_node)
-tree_newick_with_distance = treeOfLife.make_newick(new_parent_node, distance=True)
-print(f"Newick with distance {tree_newick_with_distance}")
+def create_tree(tree:Tree, nw_scores_sorted:dict, tracking_cache:dict, union_find:UnionFind):
+    node_counter = 1
+    for k, v in nw_scores_sorted.items():
+        species = k.split("_")
+        specie1 = species[0]
+        specie2 = species[1]
+        species1_root_node = tracking_cache[specie1]
+        species2_root_node = tracking_cache[specie2]
+        species1_root = union_find.find(specie1)
+        species2_root = union_find.find(specie2)
 
-with open(newick_txt_file, 'w') as f:
-    f.write(tree_newick)
-with open(newick_distance_txt_file, 'w') as f:
-    f.write(tree_newick_with_distance)
+        if species1_root!=species2_root:
+            union_find.unite(specie1, specie2)
+            node_counter = node_counter+1
+            new_parent_node = tree.createNode(name = v, value = v, left=species1_root_node, right=species2_root_node)
+            tracking_cache = tree.change_root_for_all_children(current_node=species1_root_node, new_root=new_parent_node, fast_track = tracking_cache)
+            tracking_cache = tree.change_root_for_all_children(current_node=species2_root_node, new_root=new_parent_node, fast_track = tracking_cache)
+
+    return tree, new_parent_node, tracking_cache, union_find
+
+if __name__ == "__main__":
+
+    with open(nw_json_file_path, 'r') as j:
+        nw_scores = json.loads(j.read())
+
+    with open(organisms_json_file_path, 'r') as j:
+        organisms = json.loads(j.read())
+
+    nw_scores_sorted = {k: v for k, v in sorted(nw_scores.items(), key=lambda item: item[1], reverse=True)}
+    union_find_structure = UnionFind(organisms.keys())
+    tree_of_life = Tree()
+    tracking_cache = init_tracking_cache(organisms, tree_of_life, nw_scores_sorted)
+
+    tree_of_life, root, tracking_cache, union_find_structure = create_tree(tree_of_life, nw_scores_sorted, tracking_cache, union_find_structure)
+
+    tree_newick = tree_of_life.make_newick(root)
+    tree_newick_with_distance = tree_of_life.make_newick(root, distance=True)
+
+    with open(newick_txt_file, 'w') as f:
+        f.write(tree_newick)
+    with open(newick_distance_txt_file, 'w') as f:
+        f.write(tree_newick_with_distance)
